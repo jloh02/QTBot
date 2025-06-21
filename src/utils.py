97b40a928df355e81ac152070e687b3c293e313e
Jonathan Loh <jonathanloh02@gmail.com>
2025-06-21 21:19:11 +0800

    Set deadline to time advice +12 hours for leeway

--- src/utils.py
+++ src/utils.py
@@ -4,4 +4,4 @@ import datetime
 def compute_next_deadline(frequency: str) -> datetime.date:
     now = datetime.datetime.now()
     days = int(frequency.replace("d", ""))
-    return now + datetime.timedelta(days=days)
+    return now + datetime.timedelta(days=days) + datetime.timedelta(hours=12)
